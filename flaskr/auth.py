b03841e78daa8c6b8e8185baed358085e273823c
TRẦN VĂN MẠNH <98477092+tranvannmanh@users.noreply.github.com>
2023-08-04 02:20:03 +0700

    add migrate database command feature

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,26 +1,27 @@
 import functools
 from flask import (
     Blueprint,
-    flash,
     g,
+    abort,
     redirect,
-    render_template,
     request,
     session,
     url_for,
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaskr.db import get_db
+from . import database
+from . import models
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
+db = database.db
+User = models.User
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
+        fname = request.json['fname']
         username = request.json['username']
         password = request.json['password']
-        db = get_db()
         error = None
 
         if not username:
@@ -28,91 +29,82 @@ def register():
         elif not password:
             error = 'Password is required.'
 
-        if error is None:
-            try:
-                db.execute(
-                    """
-                    INSERT INTO user (username, password) VALUES (?, ?)
-                    """,
-                    (username, generate_password_hash(password))
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-                return {
-                    "success": False,
-                    "result": None,
-                    "error": error
-                }
-            # else:
-            #     return redirect(url_for('auth.login'))
+        user_exists = User.query.filter_by(username=username).first()
+        if user_exists is not None:
+            error = f"user {username} already exists."
+    
+    if error:
+        return {
+            "error": error,
+            "success": False,
+            "result": None
+        }
+    else:
+        db.session.add(User(fname=fname, username=username, password=password))
+        db.session.commit()
 
-        # flash(error)
-    # return render_template('auth/register.html')
     return {
         "error": error,
         "success": True,
         "result": {
+            "fname": fname,
             "username": username,
             "password": generate_password_hash(password)
         }
     }
 
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.json['username']
-        password = request.json['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+# @bp.route('/login', methods=('GET', 'POST'))
+# def login():
+#     if request.method == 'POST':
+#         username = request.json['username']
+#         password = request.json['password']
+#         error = None
+#         user = 
 
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+#         if user is None:
+#             error = 'Incorrect username.'
+#         elif not check_password_hash(user['password'], password):
+#             error = 'Incorrect password.'
 
-        if error is not None:
-            return {
-                "success": True,
-                "error": error,
-                "result": None
-            }
+#         if error is not None:
+#             return {
+#                 "success": True,
+#                 "error": error,
+#                 "result": None
+#             }
 
-        # flash(error)
+#         # flash(error)
 
-    session.clear()
-    session['user_id'] = user['id']
+#     session.clear()
+#     session['user_id'] = user['id']
 
-    return {
-        "success": True,
-        "error": error,
-        "result":
-        {
-            "username": username,
-            "password": password
-        }
-    }
+#     return {
+#         "success": True,
+#         "error": error,
+#         "result":
+#         {
+#             "username": username,
+#             "password": password
+#         }
+#     }
 
 
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
+# @bp.before_app_request
+# def load_logged_in_user():
+#     user_id = session.get('user_id')
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+#     if user_id is None:
+#         g.user = None
+#     else:
+#         g.user = get_db().execute(
+#             'SELECT * FROM user WHERE id = ?', (user_id,)
+#         ).fetchone()
 
 
-@bp.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
+# @bp.route('/logout')
+# def logout():
+#     session.clear()
+#     return redirect(url_for('index'))
 
 
 def login_required(view):
--- flaskr/config.py
+++ flaskr/config.py
@@ -1,10 +1,14 @@
 from dotenv import load_dotenv
 import os
+import redis
 
 load_dotenv()
 
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///recommend_system.sqlite'
+    SESSION_TYPE="redis",
+    SESSION_USE_SIGNER=True,
+    SESSION_REDIS=redis.from_url("redis://127.0.0.1:6379")
