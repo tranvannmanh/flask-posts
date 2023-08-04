357ca2405315fbc058fdd3d921f2f90665e6c899
TRẦN VĂN MẠNH <98477092+tranvannmanh@users.noreply.github.com>
2023-08-05 01:45:14 +0700

    login with session

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -18,24 +18,26 @@ load_dotenv()
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY=os.environ['SECRET_KEY'],
-    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    #     SESSION_TYPE="redis",
-    #     SESSION_USE_SIGNER=True,
-    #     SESSION_REDIS=redis.from_url("redis://127.0.0.1:6379")
-    # )
-    app.config.from_object(config.ApplicationConfig())
+    app.config.from_mapping(
+        SECRET_KEY = os.environ["SECRET_KEY"],
+        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        SQLALCHEMY_ECHO = True,
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///recommend_system.sqlite',
+        SESSION_TYPE="redis",
+        SESSION_USE_SIGNER=True,
+        SESSION_REDIS=redis.from_url("redis://127.0.0.1:6379")
+    )
+    # app.config.from_object(config.ApplicationConfig())
     CORS(app, supports_credentials=True)
     Bcrypt(app)
     Session(app)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
