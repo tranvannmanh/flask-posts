77de2f6ae11c29beb1e2544b1c7ab61436afffc2
TRẦN VĂN MẠNH <98477092+tranvannmanh@users.noreply.github.com>
2023-08-15 00:45:58 +0700

    upgrade database

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Text, DateTime
+from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base
 from uuid import uuid4
 from . import database
@@ -26,11 +26,29 @@ class User(db.Model):
         return '<User %r>' % self.username
     
 
+class Category(db.Model):
+    __tablename__ = "CATEGORY"
+    id = Column(Integer, autoincrement=True, unique=True)
+    type = Column(String(50), primary_key=True)
+    name = Column(String(50), nullable=False)
+    posts = db.relationship("Posts", backref=db.backref('CATEGORY', lazy='joined'), lazy=True)
+
 class Posts(db.Model):
     __tablename__ = "NEWS"
     id = Column(Integer, autoincrement=True, unique=True, primary_key=True)
     title = Column(Text, nullable=False)
-    conent = Column(Text, nullable=False)
+    content = Column(Text, nullable=False)
     created = Column(DateTime, default=datetime.now())
     author = Column(String(100), nullable=True)
     image = Column(String(345), nullable=True)
+    type = Column(String(50), nullable=False)
+
+# class Recommend(db.Model):
+#     __tablename__="RECOMMENDED"
+#     user_id=Column(Integer, db.ForeignKey("USERS.id"))
+#     post_id=Column(Integer, db.ForeignKey("NEWS.id"))
+
+recommended = db.Table('RECOMMENDED',
+                       Column('user_id', Integer, ForeignKey("USERS.id"), primary_key=True),
+                       Column('post_id', Integer, ForeignKey("NEWS.id"), primary_key=True)
+                       )
